88c64115edcb2247510ca2fb1c7bade9e366d934
Fran6gift <111250094+Fran6gift@users.noreply.github.com>
2023-04-12 19:18:54 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-class MyList that inherits from list
-"""
 
+"""class MyList that inherits from list"""
 
 class MyList(list):
-    """
-    Subclass that inherits from list
-    """
+    """Subclass that inherits from list"""
 
     def print_sorted(self):
         """prints the list, but sorted"""
-        return print(sorted(self))
+         print(sorted(self))
