ced4b00db547a6db5c91eb4b6f2a87f275ee25d8
Fran6gift <111250094+Fran6gift@users.noreply.github.com>
2023-04-13 02:10:43 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-2-append_write module
-"""
+
+"""Defines a file-appending function."""
+
 
 def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
-    function that appends a string at the end of a text file (UTF8)
-    and returns the number of characters added
-    """
-    with open(filename, 'a') as af:
-        return af.write(str(text))
-#    with open(filename, 'r') as rf:
-#        f_contents = rf.readlines()
-#        print(f_contents)
-#        return(len(f_contents[-1]))
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
