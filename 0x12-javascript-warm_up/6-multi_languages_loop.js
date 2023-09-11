59c150186d2cfe2fb84c00ffe80b434320bfc15c
Fran6gift <111250094+Fran6gift@users.noreply.github.com>
2023-09-11 23:18:56 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 const length = arr.length;
 let i = 0;
 for (i = 0; i < length; i++) {
